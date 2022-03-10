1d5d03a42962f30e34f568b9e40e6635114fc1f0
Marios Kerasiotis <39958515+mariosker@users.noreply.github.com>
2022-03-10 17:53:35 +0200

    Fix block functions (hash, transaction, mine)
    Co-authored-by: Giannis Kazelidis <gmkazel@users.noreply.github.com>

--- src/Block.py
+++ src/Block.py
@@ -1,5 +1,4 @@
 import time
-from hashlib import sha256
 
 import config
 import crypto
@@ -7,7 +6,7 @@ from config import logger
 
 
 class Block:
-    """A block for the blockchain
+    """A block of the blockchain
     """
 
     def __init__(self, index: int, previous_hash: str = "") -> None:
@@ -20,25 +19,40 @@ class Block:
         self.index = index
         self.timestamp = time.time()
         self.transactions = []
-        self.nonce = None
+        self.nonce = 0
         self.previous_hash = previous_hash
         self.current_hash = self.calculate_hash()
 
     def calculate_hash(self) -> str:
-        to_hash = str(self.index) + str(self.timestamp) + str(self.nonce) + str(
+        """Calculates the hash of the block
+
+        Returns:
+            str: A hex digest of the hash
+        """
+        to_hash = str(self.timestamp) + str(self.nonce) + str(
             self.previous_hash) + "".join(
                 str(t.vars()) for t in self.transactions)
         return crypto.hash_to_str(to_hash)
 
     def add_transaction(self, transaction):
-        if len(self.transactions < config.BLOCK_CAPACITY):
+        """Adds a transaction to the block
+
+        Args:
+            transaction (Transaction): The transaction to be added
+
+        Raises:
+            ValueError: Error if block is full
+        """
+        if len(self.transactions) < config.BLOCK_CAPACITY:
             self.transactions.append(transaction)
         else:
             raise ValueError(
                 f'Block reached capacity of {config.BLOCK_CAPACITY}.')
 
     def mine_block(self):
+        """Mines the block until it begins with MINING_DIFFICULTY zeroes
+        """
         while not self.current_hash.startswith('0' * config.MINING_DIFFICULTY):
             self.nonce += 1
-            self.hash = self.calculate_hash()
+            self.current_hash = self.calculate_hash()
         logger.debug(f"Block mined with nonce {self.nonce}")
