da85594c30530a68bb6c956b1910158c77931059
SephirothK <gkazel@outlook.com>
2022-03-16 12:07:26 +0200

    add forgotten feature to register node function
    Co-authored-by: Marios Kerasiotis <mariosker@users.noreply.github.com>

--- backend/src/repository/bootstrap_node.py
+++ backend/src/repository/bootstrap_node.py
@@ -24,6 +24,16 @@ class BootstrapNode(_Node):
         self.ring.append(self.node_info)
         self.blockchain = Blockchain([self.genesis_block])
 
+    def _create_genesis_block(self) -> Block:
+        genesis_amount = config.NBC_PER_NODE * config.MAX_USER_COUNT
+        genesis_transaction = Transaction('0', self.wallet.public_key,
+                                          genesis_amount, [],
+                                          self.wallet.private_key)
+        [_, receiver_output] = genesis_transaction.transaction_outputs
+        self.wallet.unspent_transactions.append(receiver_output)
+        self.ring.update_balance(genesis_transaction)
+        return Block(0, 1, [genesis_transaction])
+
     def register_node(self, node_info: RingNode):
         """ As a bootstrap add the node to the ring and update it's id
 
@@ -37,22 +47,28 @@ class BootstrapNode(_Node):
             raise ValueError('Cannot add more nodes to the ring')
         node_info.id = len(self.ring)
         self.ring.append(node_info)
+
         if len(self.ring) == config.MAX_USER_COUNT:
-            Thread(target=self._broadcast_current_state).start()
+            Thread(target=self._init_blockchain).start()
 
         return node_info
 
-    def _create_genesis_block(self) -> Block:
-        genesis_amount = 100 * config.MAX_USER_COUNT
-        genesis_transaction = Transaction('0', self.wallet.public_key,
-                                          genesis_amount, [],
-                                          self.wallet.private_key)
-        [_, receiver_output] = genesis_transaction.transaction_outputs
-        self.wallet.unspent_transactions.append(receiver_output)
-        self.ring.update_balance(genesis_transaction)
-        return Block(0, 1, [genesis_transaction])
+    def _init_blockchain(self):
+        self._broadcast_current_state()
+        self._send_first_transactions()
 
     def _broadcast_current_state(self):
         data = {'ring': self.ring, 'blockchain': self.blockchain}
         data_pickled = pickle.dumps(data)
         self.broadcast(config.NODE_SET_INFO_URL, data_pickled)
+
+    def _send_first_transactions(self):
+        try:
+            for node in self.ring:
+                if node == self.node_info:
+                    continue
+
+                self.create_transaction(node.public_key, config.NBC_PER_NODE)
+        except ValueError as err:
+            config.logger.error(err)
+            raise ValueError('Could not create initial transactions')
