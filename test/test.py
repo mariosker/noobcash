c78ce0a55a272f27fb366866f4d4088cbd82017a
Marios Kerasiotis <39958515+mariosker@users.noreply.github.com>
2022-03-18 21:00:41 +0200

    fix tests
    Co-authored-by: Giannis Kazelidis <58084117+gmkazel@users.noreply.github.com>

--- test/test.py
+++ test/test.py
@@ -1,4 +1,5 @@
 import argparse
+from concurrent.futures import thread
 import os
 import random
 from threading import Thread
@@ -8,37 +9,42 @@ import concurrent.futures
 from functools import partial
 
 LIMIT_TRANSACTIONS = None
+
 node_data = lambda host, port: {'host': host, 'port': port}
-nodes = [node_data('localhost', p) for p in range(5000, 5004)]
+nodes = [node_data('http://localhost', p) for p in range(6000, 6005)]
 
 
 def request_transactions(node, nodes_count):
+    print(f"./transactions/{nodes_count}nodes/transactions{node}.txt")
     host = nodes[node]['host']
     port = nodes[node]['port']
-    count = 0
-    with open(f"transactions/{nodes_count}nodes/transactions{node}.txt",
+    print('hello')
+    with open(f"./transactions/{nodes_count}nodes/transactions{node}.txt",
               'r') as f:
-        try:
-            for line in f:
-                count += 1
-                id_text, amount = line.split(" ")
-                id = int(id_text[-1])
-                amount = int(amount)
+        for line in f:
+            id_text, amount = line.split(" ")
+            id = int(id_text[-1])
+            amount = int(amount)
 
-                data = {'node_id': id, 'amount': amount}
-                requests.post(f'{host}:{port}/transactions', params=data)
-                time.sleep(random.random() * 5)
-                if LIMIT_TRANSACTIONS and count > LIMIT_TRANSACTIONS:
-                    break
-        except Exception as err:
-            print(err)
+            data = {'node_id': id, 'amount': amount}
+            print(node, data)
+            requests.post(f'{host}:{port}/transactions', data=data)
+            time.sleep(random.random() * 10)
 
 
 def main(nodes_count=5):
-    with concurrent.futures.ThreadPoolExecutor(
-            max_workers=nodes_count) as executor:
-        executor.map(partial(request_transactions, nodes_count=nodes_count),
-                     range(nodes_count))
+    threads = []
+    for i in range(5):
+        threads.append(
+            Thread(target=request_transactions, args=(
+                i,
+                nodes_count,
+            )))
+
+    for i in range(5):
+        threads[i].start()
+    for i in range(5):
+        threads[i].join()
 
 
 if __name__ == "__main__":
